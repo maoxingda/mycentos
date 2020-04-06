6de3339c79f7c27c23ec3e1c2eb9f714b7c55d46
maoxingda <954192476@qq.com>
2020-04-06 20:50:39 +0800

    add xsync.py

---
+++ util.py
@@ -0,0 +1,93 @@
+﻿#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import logging
+import os
+import re
+import sys
+import termios
+import tty
+
+from subprocess import call
+
+LOG_FORMAT = "[%(asctime)s %(levelname)8s] %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+
+
+def logcall(args):
+    logging.info(args)
+    call(args, shell=True)
+
+
+def yesorno():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def putenv(interactive: bool, profile_path: str, env_name: str, abspath: str, *relpaths):
+    env_name = env_name.upper()
+
+    if re.match(r'^\d', env_name):
+        raise ValueError('the evironment variable name can not start with digit：' + env_name)
+
+    select_lines = []
+    if os.path.exists(profile_path):
+        with open(profile_path, 'r') as conf:
+            initial_lines = conf.readlines()
+            find = False
+            for lineno in range(len(initial_lines)):
+                what = re.search(rf'^#\s+\b{env_name}\b', initial_lines[lineno])
+                if not find and what:
+                    find = True
+                    if interactive:
+                        logging.info(f'[{env_name}={os.environ[env_name]}] already existed'
+                                     ', reset or stay the same (y/n)')
+                        if 'y' != yesorno():
+                            logging.info(f'give up setting evironment variable {env_name}')
+                            sys.exit()
+
+                if not what:
+                    select_lines.append(initial_lines[lineno])
+
+    select_lines.append(f'# {env_name}\n')
+    select_lines.append(f'export {env_name}={abspath}\n')
+    select_lines.append(f'export PATH=$PATH:${env_name}/bin\n')
+    for path in relpaths:
+        select_lines.append(f'export PATH=$PATH:${env_name}/{path}\n')
+
+    with open(profile_path, 'w') as conf:
+        conf.writelines(select_lines)
+
+
+import time
+
+
+def progress_bar(num):
+    j = "#";
+    k = "=";
+    t = "|/-\\";  # s = " " * (num + 1)
+
+    for i in range(0, num + 1):
+        j += "#";
+        k += "=";
+        s = ("=" * i) + (" " * (num - i))
+
+        # print(int(i/num*100), end='%\r')
+        # print('%.2f' % (i/num*100), end='%\r')
+        # print('%.2f' % (i*100/num), end='%\r')
+        # print('complete percent:', time.strftime("%Y-%m-%d %H:%M:%S", \
+        #        time.localtime()), int((i/num)*100), end='%\r')
+        # print(str(int(i/num*100)) + '% ' + j + '->', end='\r')
+        # print(k + ">" + str(int(i/num*100)), end='%\r')
+        # print("[%s]" % t[i%4], end='\r')
+        # print("[%s][%s][%.2f" % (t[i%4], k, (i/num*100)), "%]", end='\r')
+        print("[%s][%s][%.2f" % (t[i % 4], s, (i / num * 100)), "%]", end='\r')
+
+        time.sleep(0.1)
+
+    print()
---
+++ xsync.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import argparse
+import re
+import sys
+
+from util import logcall
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-f', '--file', required=True
+                        , help='the file name will be created')
+    parser.add_argument('-w', '--hosts', required=True
+                        , help='the host list where the file will be sync to'
+                               ', hostname[start-end] or support hostname1,hostname2,...')
+
+    args = parser.parse_args()
+
+    hosts = []
+
+    if re.fullmatch(r'(\w+)(,\w+)*', args.hosts):
+        hosts = args.hosts.split(',')
+
+    what = re.fullmatch(r'(\w+)\[(\d+)-(\d+)\]', args.hosts)
+    if what:
+        beg = what.group(2)
+        end = what.group(3)
+        for host in range(int(beg), int(end) + 1):
+            hosts.append(what.group(1) + str(host))
+
+    if len(hosts) == 0:
+        print('no host was specified')
+        sys.exit()
+
+    sync_lines = []
+    hspace1 = '    '
+    hspace2 = '        '
+    hspace3 = '            '
+    sync_lines.append(f'{hspace1}for i, directory in enumerate(sys.argv):\n')
+    sync_lines.append(f'{hspace2}if i == 0:\n')
+    sync_lines.append(f'{hspace3}continue\n\n')
+
+    sync_lines.append(f'{hspace2}if os.path.isfile(directory):\n')
+    sync_lines.append(f'{hspace3}print(\'skipping file: \'' + ' + directory' +')\n')
+    sync_lines.append(f'{hspace3}continue\n\n')
+
+    sync_lines.append(f'{hspace2}directory = os.path.abspath(directory)\n\n')
+    for host in hosts:
+        sync_lines.append(f'{hspace2}print(\'==============[{host}]==============\')\n')
+        sync_lines.append(f'{hspace2}subprocess.call(f\'ssh ' + host + ' "mkdir -p {directory}"\', shell=True)\n')
+        sync_lines.append(f'{hspace2}subprocess.call(f\'' + 'rsync -avz {directory}/ '
+                          + host + ':{directory}\', shell=True)\n\n')
+
+    lines = ['#! /usr/bin/python3\n',
+             '# -*- coding: utf-8 -*-\n\n',
+             'import os\n',
+             'import sys\n',
+             'import subprocess\n\n',
+
+             'if __name__ == \'__main__\':\n',
+             '    if len(sys.argv) < 2:\n',
+             '        print(\'usage: xsync.py directory [directory]...\')\n',
+             '        sys.exit()\n\n']
+
+    lines.extend(sync_lines)
+
+    with open(args.file, 'w') as file:
+        file.writelines(lines)
+
+    logcall(f'chmod +x {args.file}')
