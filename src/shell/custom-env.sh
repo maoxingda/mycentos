3d83ecc77fa260a1303a6ba3a319a5e8793e77bf
maoxd <954194276@qq.com>
2020-04-08 22:31:05 +0800

    cron push...

--- src/shell/custom-env.sh
+++ src/shell/custom-env.sh
@@ -3,19 +3,14 @@ alias qfind='sudo updatedb; locate'
 
 export PS1="\[\e[37;40m\][\[\e[34;40m\]\u\[\e[37;40m\]@\[\e[35;40m\]\h \[\e[36;40m\]\W\[\e[0m\]]\\$ "
 
-# colorful man page
-function man()
-{
-    env \
-    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-    LESS_TERMCAP_md=$(printf "\e[1;31m") \
-    LESS_TERMCAP_me=$(printf "\e[0m") \
-    LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-    LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
-    man "$@"
-}
+# JAVA_HOME
+export JAVA_HOME=/opt/module/jdk1.8.0_212
+export PATH=$PATH:$JAVA_HOME/bin
+
+# HADOOP_HOME
+export HADOOP_HOME=/opt/module/hadoop-3.1.3
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
 
 # KAFKA_HOME
 export KAFKA_HOME=/opt/module/kafka_2.11-0.11.0.0
