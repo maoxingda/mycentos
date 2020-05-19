beb559a7eb48fce8143447eff20142b5acfd21ad
maoxingda <954192476@qq.com>
2020-05-20 06:18:00 +0800

    hide LocalPath and RmotePath

--- src/xhdfs.py
+++ src/xhdfs.py
@@ -1,109 +1,9 @@
 #! /bin/python3
 import os
 import re
-import sys
 import readline
-from abc import abstractmethod
+import sys
 from subprocess import call, check_output
-
-
-# print(os.getcwd())
-# os.chdir(os.path.expandvars('${HOME}'))
-# print(os.getcwd())
-# os.chdir(os.pardir)
-# print(os.getcwd())
-# sys.exit()
-
-
-# class Shape:
-#     def m1(self):
-#         print('Shape')
-#
-#
-# class Circle(Shape):
-#
-#     def m1(self):
-#         print('Circle')
-#
-#
-# shape = Shape()
-# shape.m1()
-# shape = Circle()
-# shape.m1()
-# sys.exit()
-
-# class Person:
-#
-#     def __init__(self, aid):
-#         self._id = aid
-#
-#
-# class Student(Person):
-#
-#     def __init__(self, aid, name, age):
-#         super().__init__(aid)
-#         self.__name = name
-#         self.age = age
-#
-#     def __str__(self) -> str:
-#         return f'Student({self._id}, {self.__name}, {self.age})'
-#
-#
-# s1 = Student(1, 'maoxd', 30)
-# print(s1)
-# # print(s1.__name)
-# print(s1._id)
-# print(s1.age)
-# sys.exit()
-
-
-class XPath(object):
-    curdir = '/'
-
-    @staticmethod
-    def getcwd():
-        return XPath.curdir
-
-    @staticmethod
-    def join(path):
-        return os.path.join(XPath.curdir, path)
-
-    @staticmethod
-    def chdir(path):
-        if path == '.':
-            XPath.curdir = os.path.dirname(XPath.curdir)
-        elif path == '..':
-            XPath.curdir = os.path.dirname(XPath.curdir)
-            XPath.curdir = os.path.dirname(XPath.curdir)
-        elif path == 'cd':
-            XPath.curdir = '/'
-        elif path.startswith('cd '):
-            XPath.curdir = os.path.join(XPath.curdir, path[3:])
-
-
-def dir_layout():
-    local_bname = os.path.basename(os.getcwd())
-    if local_bname == '':
-        local_bname = '/'
-
-    remote_bname = os.path.basename(XPath.getcwd())
-    if remote_bname == '':
-        remote_bname = '/'
-
-    prompt = '>'
-    if mode == 2:
-        prompt += '>>'
-
-    return f'\n[{local_bname}:{remote_bname}]{prompt} '
-
-
-def mk_remote_choices():
-    rchs = check_output(['hdfs', 'dfs', '-ls', '-C', XPath.getcwd()]).decode('utf-8').split('\n')[:-1]
-    return [rch.split('/')[-1] for rch in rchs]
-
-
-def catcmd(acmd, *args):
-    return acmd + ' ' + ' '.join(args)
 
 
 def logcall(acmd):
@@ -111,108 +11,18 @@ def logcall(acmd):
         if enable_log:
             print(acmd)
         call(acmd, shell=True)
-    else:
+    elif mode == 1:
         if enable_log:
             print(f'>>> hdfs dfs -{acmd}')
         call(f'hdfs dfs -{acmd}', shell=True)
-
-
-def completer(text, state):
-    # options = [x for x in ['exit'] + local_choices + remote_choices if x.find(text) != -1]
-    options = [x for x in ['exit'] + local_choices + remote_choices if x.startswith(text)]
-    try:
-        return options[state]
-    except IndexError:
-        return None
 
 
 def unkown_cmd(acmd):
     print(f'unkown command ---> {acmd}')
 
 
-def choice_layout():
-    if enable_log:
-        if len(local_choices) > 0:
-            print('\n=============local choices=============')
-        for choice in local_choices:
-            print(choice, end=' ')
-
-        if len(remote_choices) > 0:
-            print('\n\n=============remote choices============')
-        for choice in remote_choices:
-            print(choice, end=' ')
-
-        if len(local_choices) > 0 or len(remote_choices) > 0:
-            print()
-    # print(readline.get_completer_delims())
-
-
-def join(path):
-    return os.path.join(os.getcwd(), path)
-
-
-def mkdir():
-    global remote_choices
-    what = re.search(r'^\s*md\s+'
-                     r'(?P<option>(-p\s+)?)'
-                     r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
-    if what:
-        option = what.group('option')
-        root = what.group('root')
-        arg = what.group('prefix') + what.group('suffix')
-        if root:
-            if option:
-                logcall(f'mkdir -p {arg}')
-            else:
-                logcall(f'mkdir {arg}')
-        else:
-            if option:
-                logcall(f'mkdir -p {XPath.join(arg)}')
-            else:
-                logcall(f'mkdir {XPath.join(arg)}')
-    remote_choices = mk_remote_choices()
-
-
-def init():
-    global local_choices, remote_choices
-
-    remote_choices += mk_remote_choices()
-    local_choices += check_output(['ls', os.getcwd()]).decode('utf-8').split('\n')[:-1]
-
-    readline.set_completer(completer)
-    readline.parse_and_bind("tab: complete")
-
-
-def rmdir():
-    global update, remote_choices
-
-    what = re.search(r'^\s*rm\s+'
-                     r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
-
-    update = True
-
-    if what:
-        root = what.group('root')
-        arg = what.group('prefix') + what.group('suffix')
-
-        if root:
-            logcall(f'rm -r {arg}')
-        else:
-            logcall(f'rm -r {XPath.join(arg)}')
-
-    # wildcard
-    elif re.search(r'^\s*rm\s+\*\.\w+', cmd):
-        logcall(cmd)
-    else:
-        update = False
-
-    if update:
-        remote_choices = mk_remote_choices()
-
-
 def put():
-    global cmd, remote_choices
-
+    global cmd
     cmd = cmd[4:]
 
     if cmd[0] == '/':
@@ -221,27 +31,92 @@ def put():
             if cmd[idx + 1] == '/':
                 logcall(f'put {cmd}')
             else:
-                logcall(f'put {cmd[:idx]} {XPath.join(cmd[idx + 1:])}')
+                logcall(f'put {cmd[:idx]} {xpath.join(cmd[idx + 1:])}')
         else:
-            logcall(f'put {cmd} {XPath.getcwd()}')
+            logcall(f'put {cmd} {remote_path.cwd}')
     else:
         idx = cmd.find(' ')
         if idx != -1:
             if cmd[idx + 1] == '/':
-                logcall(f'put {join(cmd[:idx])} {cmd[idx + 1:]}')
+                logcall(f'put {xpath.join(cmd[:idx])} {cmd[idx + 1:]}')
             else:
-                logcall(f'put {join(cmd[:idx])} {XPath.join(cmd[idx + 1:])}')
+                logcall(f'put {xpath.join(cmd[:idx])} {xpath.join(cmd[idx + 1:])}')
         else:
-            logcall(f'put {join(cmd)} {XPath.getcwd()}')
-
-    remote_choices = mk_remote_choices()
+            logcall(f'put {xpath.join(cmd)} {remote_path.cwd}')
 
 
-class XXPath:
+class Wacher:
+
+    def subcribe(self, msg):
+        pass
+
+
+class PathWacherCompleter(Wacher):
+
+    def __init__(self):
+        readline.set_completer(PathWacherCompleter.completer)
+        readline.parse_and_bind("tab: complete")
+
+        self.__remote_choices = check_output([
+            'hdfs', 'dfs', '-ls', '-C', '/']).decode('utf-8').split('\n')[:-1]
+        self.__remote_choices = [rch.split('/')[-1] for rch in self.__remote_choices]
+        self.__local_choices = check_output(['ls', os.getcwd()]).decode('utf-8').split('\n')[:-1]
+
+    @staticmethod
+    def completer(text, state):
+        try:
+            return [ch for ch in ['exit'] + wacher.choices() if ch.startswith(text)][state]
+        except IndexError:
+            return None
+
+    def mk_local_choices(self, path):
+        self.__local_choices = check_output(['ls', path]).decode('utf-8').split('\n')[:-1]
+
+    def mk_remote_choices(self, path):
+        self.__remote_choices = check_output(['hdfs', 'dfs', '-ls', '-C', path]).decode('utf-8').split('\n')[:-1]
+        self.__remote_choices = [rch.split('/')[-1] for rch in self.__remote_choices]
+
+    def choices(self):
+        return self.__local_choices + self.__remote_choices
+
+    def subcribe(self, msg):
+        if mode == 1:
+            self.mk_remote_choices(msg)
+        elif mode == 2:
+            self.mk_local_choices(msg)
+
+    def choice_layout(self):
+        if enable_log:
+            if len(self.__local_choices) > 0:
+                print('\n=============local choices=============')
+            for choice in self.__local_choices:
+                print(choice, end=' ')
+
+            if len(self.__remote_choices) > 0:
+                print('\n\n=============remote choices============')
+            for choice in self.__remote_choices:
+                print(choice, end=' ')
+
+            if len(self.__local_choices) > 0 or len(self.__remote_choices) > 0:
+                print()
+        # print(readline.get_completer_delims())
+
+
+class BasePath:
 
     def __init__(self, cwd, oldpwd):
+        self.__init = True
         self.cwd = cwd
         self.oldpwd = oldpwd
+        self.__wachers = []
+        self.__init = False
+
+    def add_wacher(self, awacher):
+        self.__wachers.append(awacher)
+
+    def dscribe(self, path):
+        for iwacher in self.__wachers:
+            iwacher.subcribe(path)
 
     @property
     def cwd(self):
@@ -249,6 +124,8 @@ class XXPath:
 
     @cwd.setter
     def cwd(self, val):
+        if not self.__init and self.__cwd != val:
+            self.dscribe(val)
         self.__cwd = val
 
     @property
@@ -270,6 +147,22 @@ class XXPath:
 
     def cdoldpwd(self):
         pass
+
+    def join(self, path):
+        return os.path.join(self.cwd, path)
+
+    @staticmethod
+    def ischdircmd(acmd):
+        if re.fullmatch(r'\s*\.\s*', acmd) or \
+                re.fullmatch(r'\s*\.\.\s*', acmd) or \
+                re.fullmatch(r'\s*cd\s*', acmd) or \
+                re.fullmatch(r'\s*cd\s+-\s*', acmd) or \
+                re.fullmatch(r'\s*cd\s*'
+                             r'(?P<root>/?)'
+                             r'(?P<arg>\w+(?:/\w+)*)/?\s*', acmd):
+            return True
+        else:
+            return False
 
     def chdir(self, acmd):
         # .
@@ -316,7 +209,7 @@ class XXPath:
                 unkown_cmd(acmd)
 
 
-class LocalPath(XXPath):
+class LocalPath(BasePath):
 
     def __init__(self, cwd, oldpwd):
         super().__init__(cwd, oldpwd)
@@ -335,7 +228,7 @@ class LocalPath(XXPath):
         os.chdir(self.oldpwd)
 
 
-class RemotePath(XXPath):
+class RemotePath(BasePath):
 
     def __init__(self, cwd, oldpwd):
         super().__init__(cwd, oldpwd)
@@ -343,37 +236,109 @@ class RemotePath(XXPath):
     def cdhome(self):
         self.cwd = '/'
 
+    @staticmethod
+    def ismkdir():
+        return re.fullmatch(r'\s*md\s+'
+                            r'(?P<option>(?:-p\s+)?)'
+                            r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
 
-p = RemotePath('/', '/')
-# p = LocalPath(os.getcwd(), os.getcwd())
+    @staticmethod
+    def mkdir():
+        what = re.fullmatch(r'\s*md\s+'
+                            r'(?P<option>(?:-p\s+)?)'
+                            r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
+        if what:
+            option = what.group('option')
+            root = what.group('root')
+            arg = what.group('prefix') + what.group('suffix')
+            if root:
+                if option:
+                    logcall(f'mkdir -p {arg}')
+                else:
+                    logcall(f'mkdir {arg}')
+            else:
+                if option:
+                    logcall(f'mkdir -p {remote_path.join(arg)}')
+                else:
+                    logcall(f'mkdir {remote_path.join(arg)}')
 
-while True:
+    @staticmethod
+    def isrmdir():
+        return re.fullmatch(r'^\s*rm\s+'
+                            r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
 
-    print(p.cwd)
+    @staticmethod
+    def rmdir():
+        what = re.fullmatch(r'^\s*rm\s+'
+                            r'(?P<root>/?)(?P<prefix>\w+)(?P<suffix>(/\w+)*)', cmd)
 
-    tcmd = input('>>> ')
+        if what:
+            root = what.group('root')
+            arg = what.group('prefix') + what.group('suffix')
 
-    if tcmd == 'exit':
-        sys.exit()
+            if root:
+                logcall(f'rm -r {arg}')
+            else:
+                logcall(f'rm -r {remote_path.join(arg)}')
 
-    p.chdir(tcmd)
+        # wildcard
+        elif re.fullmatch(r'^\s*rm\s+\*\.\w+', cmd):
+            logcall(cmd)
+
+
+class Path:
+
+    def __init__(self, lpath: BasePath, rpath: BasePath):
+        self.__local_path: BasePath = lpath
+        self.__remote_path: BasePath = rpath
+
+    def add_wacher(self, lwacher: Wacher, rwacher: Wacher):
+        self.__local_path.add_wacher(lwacher)
+        self.__remote_path.add_wacher(rwacher)
+
+    def dir_layout(self):
+        local_bname = os.path.basename(self.__local_path.cwd)
+        if local_bname == '':
+            local_bname = '/'
+
+        remote_bname = os.path.basename(self.__remote_path.cwd)
+        if remote_bname == '':
+            remote_bname = '/'
+
+        pmode = 'hdfs'
+        prompt = '>'
+        if mode == 2:
+            prompt += '>>'
+            pmode = 'local'
+
+        return f'\n[{pmode}][{local_bname}:{remote_bname}]{prompt} '
+
+    def join(self, path):
+        if mode == 1:
+            self.__local_path.join(path)
+        elif mode == 2:
+            self.__remote_path.join(path)
 
 
 if __name__ == '__main__':
 
     mode = 1
-    update = True
-    local_choices = []
-    remote_choices = []
-    unkown_cmd = 'unkown command'
     enable_log = len(sys.argv) > 1 and sys.argv[1] == '1'
 
-    init()
+    wacher = PathWacherCompleter()
+
+    local_path = LocalPath(os.getcwd(), os.getcwd())
+    remote_path = RemotePath('/', '/')
+
+    local_path.add_wacher(wacher)
+    remote_path.add_wacher(wacher)
+
+    xpath = Path(local_path, remote_path)
 
     while True:
 
-        choice_layout()
-        cmd = input(dir_layout())
+        wacher.choice_layout()
+        cmd = input(xpath.dir_layout())
 
         try:
             if cmd == 'exit':
@@ -388,59 +353,43 @@ if __name__ == '__main__':
             elif mode == 2:
 
                 # change dir
-                update = True
-                if cmd == '.':
-                    os.chdir(os.path.abspath(os.pardir))
-                elif cmd == '..':
-                    os.chdir(os.path.abspath(os.pardir))
-                    os.chdir(os.path.abspath(os.pardir))
-                elif cmd == 'cd':
-                    os.chdir(os.environ['HOME'])
-                elif cmd == 'cd -':
-                    os.chdir(os.environ['OLDPWD'])
-                elif cmd.startswith('cd '):
-                    os.chdir(join(cmd[3:]))
-
+                if BasePath.ischdircmd(cmd):
+                    local_path.chdir(cmd)
                 # others
                 else:
                     logcall(cmd)
-                    update = False
-
-                if update is True:
-                    local_choices = check_output(['ls', os.getcwd()]).decode('utf-8').split('\n')[:-1]
 
             # hdfs commands
             else:
 
                 # change dir
                 if cmd == 'pwd':
-                    print(XPath.getcwd())
-                elif re.search(r'^\s*(?:\.|\.\.|cd|cd\s+)', cmd):
-                    XPath.chdir(cmd)
-                    remote_choices = mk_remote_choices()
+                    print(remote_path.cwd)
+                elif BasePath.ischdircmd(cmd):
+                    remote_path.chdir(cmd)
 
                 # create dir
-                elif re.search(r'^\s*md\s+', cmd):
-                    mkdir()
+                elif remote_path.ismkdir():
+                    remote_path.mkdir()
 
                 # touch file
                 elif cmd.startswith('touch '):
                     pass
 
                 # del dir or file
-                elif re.search(r'^\s*rm\s+', cmd):
-                    rmdir()
+                elif remote_path.isrmdir():
+                    remote_path.rmdir()
 
                 # list dir content
                 elif cmd == 'ls':
-                    logcall(f'ls {XPath.getcwd()}')
+                    logcall(f'ls {remote_path.cwd}')
                 elif cmd == 'lsc':
-                    logcall(f'ls -C {XPath.getcwd()}')
+                    logcall(f'ls -C {remote_path.cwd}')
                 elif cmd.startswith('ls '):
                     if cmd[3] == '/':
                         logcall(f'ls {cmd[3:]}')
                     else:
-                        logcall(f'ls {XPath.join(cmd[3:])}')
+                        logcall(f'ls {remote_path.join(cmd[3:])}')
 
                 # cat file content
                 elif cmd.startswith('cat '):
@@ -448,7 +397,7 @@ if __name__ == '__main__':
                     if cmd[0] == '/':
                         logcall(f'cat {cmd}')
                     else:
-                        logcall(f'cat {XPath.join(cmd)}')
+                        logcall(f'cat {remote_path.join(cmd)}')
 
                 # put or get file
                 elif cmd.startswith('put '):
@@ -458,7 +407,7 @@ if __name__ == '__main__':
                     if cmd[0] == '/':
                         logcall(f'get {cmd}')
                     else:
-                        logcall(f'get {XPath.join(cmd)}')
+                        logcall(f'get {remote_path.join(cmd)}')
 
                 # others
                 else:
